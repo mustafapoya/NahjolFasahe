92fb188559004336f163fb623965cc8661b7b310
MustafaPoya <mustafapoyaabc@gmail.com>
2022-11-20 20:16:02 +0430

    add sqlite dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,12 @@
 module net.golbarg.nahjolfasahe {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     requires org.controlsfx.controls;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
+    requires org.xerial.sqlitejdbc;
 
     opens net.golbarg.nahjolfasahe to javafx.fxml;
     exports net.golbarg.nahjolfasahe;
