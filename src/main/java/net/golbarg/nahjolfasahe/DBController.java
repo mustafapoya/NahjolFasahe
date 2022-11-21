be55e5a2b45e435f3b449ab9d16ec0ee507e83d3
MustafaPoya <mustafapoyaabc@gmail.com>
2022-11-21 08:26:17 +0430

    add query data methods
    - get all hadises
    - get hadises by category
    - get categories
    - get sub categories by category

--- src/main/java/net/golbarg/nahjolfasahe/DBController.java
+++ src/main/java/net/golbarg/nahjolfasahe/DBController.java
@@ -39,6 +39,102 @@ public class DBController {
         return connection;
     }
 
-    
+    public static ObservableList<Hadis> getAllHadis() {
+        ObservableList<Hadis> hadisList = FXCollections.observableArrayList();
+        String query = "SELECT * FROM " + TABLE_HADIS;
+
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet result = statement.executeQuery();
+
+            while(result.next()) {
+                long id = result.getLong("id");
+                Category category = new Category(result.getString("category"));
+                Category subCategory = new Category(result.getString("sub_category"));
+                String hadisText = result.getString("hadis_text");
+
+                hadisList.add(new Hadis(id, category, subCategory, hadisText));
+
+
+            }
+        } catch(SQLException e) {
+            Logger.getAnonymousLogger().log(
+                    Level.SEVERE,
+                    LocalDateTime.now() + ": Could not load Hadises from database ");
+        }
+
+        return hadisList;
+    }
+
+    public static ObservableList<Hadis> getHadisOf(String category) {
+        ObservableList<Hadis> hadisList = FXCollections.observableArrayList();
+        String query = "SELECT * FROM " + TABLE_HADIS + " WHERE category = ?;";
+
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, category);
+            ResultSet result = statement.executeQuery();
+
+            while(result.next()) {
+                long id = result.getLong("id");
+                Category category1 = new Category(result.getString("category"));
+                Category subCategory = new Category(result.getString("sub_category"));
+                String hadisText = result.getString("hadis_text");
+
+                hadisList.add(new Hadis(id, category1, subCategory, hadisText));
+
+            }
+        } catch(SQLException e) {
+            Logger.getAnonymousLogger().log(
+                    Level.SEVERE,
+                    LocalDateTime.now() + ": Could not load Hadises by category from database");
+        }
+
+        return hadisList;
+    }
+
+    public static ObservableList<Category> getAllCategory() {
+        ObservableList<Category> categoryList = FXCollections.observableArrayList();
+        String query = "select distinct category from  " + TABLE_HADIS;
+
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet result = statement.executeQuery();
+
+            while(result.next()) {
+                Category category = new Category(result.getString("category"));
+                categoryList.add(category);
+            }
+        } catch(SQLException e) {
+            Logger.getAnonymousLogger().log(
+                    Level.SEVERE,
+                    LocalDateTime.now() + ": Could not load Hadis Category from database ");
+        }
+
+        return categoryList;
+    }
+
+
+    public static ObservableList<Category> getAllSubCategory(String category) {
+        ObservableList<Category> subCategoryList = FXCollections.observableArrayList();
+        String query = "select distinct sub_category from "+ TABLE_HADIS +" where category = ?;";
+
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, category);
+
+            ResultSet result = statement.executeQuery();
+
+            while(result.next()) {
+                subCategoryList.add(new Category(result.getString("sub_category")));
+            }
+        } catch(SQLException e) {
+            Logger.getAnonymousLogger().log(
+                    Level.SEVERE,
+                    LocalDateTime.now() + ": Could not load Hadis Category from database ");
+        }
+
+        return subCategoryList;
+    }
 
 }
