6ef21fe2c851aa6c1a952c23445be643e9fc4746
MustafaPoya <mustafapoyaabc@gmail.com>
2022-11-21 09:25:54 +0430

    test db

--- src/main/java/net/golbarg/nahjolfasahe/TestDb.java
+++ src/main/java/net/golbarg/nahjolfasahe/TestDb.java
@@ -1,5 +1,9 @@
 package net.golbarg.nahjolfasahe;
 
+import javafx.collections.ObservableList;
+import net.golbarg.nahjolfasahe.models.Category;
+import net.golbarg.nahjolfasahe.models.Hadis;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -7,26 +11,32 @@ import java.sql.ResultSet;
 
 public class TestDb {
     public static void main(String[] args) {
-        try {
-            Class.forName("org.sqlite.JDBC");
-            DriverManager.registerDriver(new org.sqlite.JDBC());
+        DBController.checkDriver();
+        ObservableList<Category> list = DBController.getAllSubCategory("آخرالزمان");
 
-            Connection connection = DriverManager.getConnection("jdbc:sqlite:" + TestDb.class.getResource("hadis_db.db").toExternalForm());
-            String query = "select * from hadises;";
-
-            PreparedStatement statement = connection.prepareStatement(query);
-            ResultSet results = statement.executeQuery();
-
-            System.out.println("سلام بر شما");
-            while(results.next()) {
-                System.out.println(results.getString("category"));
-            }
-
-
-        } catch(Exception e) {
-            e.printStackTrace();
+        for(int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
         }
 
+//        try {
+//            Class.forName("org.sqlite.JDBC");
+//            DriverManager.registerDriver(new org.sqlite.JDBC());
+//
+//            Connection connection = DriverManager.getConnection("jdbc:sqlite:" + TestDb.class.getResource("hadis_db.db").toExternalForm());
+//            String query = "select * from hadises;";
+//
+//            PreparedStatement statement = connection.prepareStatement(query);
+//            ResultSet results = statement.executeQuery();
+//
+//            System.out.println("سلام بر شما");
+//            while(results.next()) {
+//                System.out.println(results.getString("category"));
+//            }
+//
+//
+//        } catch(Exception e) {
+//            e.printStackTrace();
+//        }
 
     }
 }
