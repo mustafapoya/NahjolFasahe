a79272a2cebf0ce1ac17b2c0b069068904e399cb
MustafaPoya <mustafapoyaabc@gmail.com>
2022-11-21 11:29:22 +0430

    create and assign ids

--- src/main/java/net/golbarg/nahjolfasahe/MainViewController.java
+++ src/main/java/net/golbarg/nahjolfasahe/MainViewController.java
@@ -1,13 +1,49 @@
 package net.golbarg.nahjolfasahe;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.util.Callback;
+import net.golbarg.nahjolfasahe.convertor.CategoryConvertor;
+import net.golbarg.nahjolfasahe.models.Category;
+import org.controlsfx.control.SearchableComboBox;
+import org.controlsfx.control.StatusBar;
 
-public class MainViewController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainViewController implements Initializable {
 
     @FXML
     private BorderPane root;
+    @FXML
+    private VBox vbTop;
 
+    @FXML
+    private SplitPane splitPane;
+    // right panel
+    @FXML
+    private BorderPane borderPaneCategory;
+    @FXML
+    private TextField txtSearchCategory;
+    @FXML
+    private ListView<Category> listViewCategory;
+    // left panel
+    @FXML
+    private BorderPane borderPaneContent;
+    @FXML
+    private TextField txtSearchHadis;
+    @FXML
+    private TextArea txtHadis;
 
+    @FXML
+    private VBox vbBottom;
+    @FXML
+    private StatusBar statusBar;
+
+    
 }
