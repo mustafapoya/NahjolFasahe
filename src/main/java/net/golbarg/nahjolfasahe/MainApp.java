a1fe0d3398c02be679fdd4250afea323000dbe6c
MustafaPoya <mustafapoyaabc@gmail.com>
2022-11-21 09:25:34 +0430

    re organize code

--- src/main/java/net/golbarg/nahjolfasahe/MainApp.java
+++ src/main/java/net/golbarg/nahjolfasahe/MainApp.java
@@ -10,9 +10,10 @@ import java.io.IOException;
 public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("main-view.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("نهج الفصاحه");
         stage.setScene(scene);
         stage.show();
     }
