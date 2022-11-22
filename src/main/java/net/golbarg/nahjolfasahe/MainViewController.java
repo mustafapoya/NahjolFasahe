af98f75d0a43610ba1b9e29807c0ffb8f84b291e
MustafaPoya <mustafapoyaabc@gmail.com>
2022-11-22 13:45:19 +0430

    add filter to listview of category

--- src/main/java/net/golbarg/nahjolfasahe/MainViewController.java
+++ src/main/java/net/golbarg/nahjolfasahe/MainViewController.java
@@ -1,16 +1,17 @@
 package net.golbarg.nahjolfasahe;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.util.Callback;
-import net.golbarg.nahjolfasahe.convertor.CategoryConvertor;
 import net.golbarg.nahjolfasahe.models.Category;
-import org.controlsfx.control.SearchableComboBox;
 import org.controlsfx.control.StatusBar;
 
 import java.net.URL;
@@ -50,12 +51,13 @@ public class MainViewController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         allCategories = DBController.getAllCategory();
-        listViewCategory.setItems(allCategories);
+        FilteredList<Category> filteredList= new FilteredList<>(allCategories, data -> true);
+
+        listViewCategory.setItems(filteredList);
         listViewCategory.setCellFactory(new Callback<ListView<Category>, ListCell<Category>>() {
             @Override
             public ListCell<Category> call(ListView<Category> param) {
                 final Label leadLbl = new Label();
-                final Tooltip tooltip = new Tooltip();
                 final ListCell<Category> cell = new ListCell<Category>() {
                     @Override
                     protected void updateItem(Category item, boolean empty) {
@@ -64,14 +66,38 @@ public class MainViewController implements Initializable {
                         if (item != null) {
                             leadLbl.setText(item.getTitle());
                             setText(item.getTitle());
-                            tooltip.setText(item.getTitle());
-                            setTooltip(tooltip);
                         } else {
-
+                            leadLbl.setText("");
+                            setText("");
                         }
                     }
                 };
                 return cell;
+            }
+        });
+
+        txtSearchCategory.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                filteredList.setPredicate(data -> {
+                    if(newValue == null || newValue.isEmpty()) {
+                        return true;
+                    }
+                    String search = newValue.toLowerCase();
+
+                    if(data.getTitle().trim().contains(search)) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                });
+            }
+        });
+
+        listViewCategory.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Category>() {
+            @Override
+            public void changed(ObservableValue<? extends Category> observable, Category oldValue, Category newValue) {
+                
             }
         });
     }
