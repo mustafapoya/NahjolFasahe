60952252265efb327165adf21d905d69410dc16c
MustafaPoya <mustafapoyaabc@gmail.com>
2022-11-23 13:31:03 +0430

    Make the stage accessable

--- src/main/java/net/golbarg/nahjolfasahe/MainApp.java
+++ src/main/java/net/golbarg/nahjolfasahe/MainApp.java
@@ -1,6 +1,8 @@
 package net.golbarg.nahjolfasahe;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -8,15 +10,20 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class MainApp extends Application {
+    public static Stage stage;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("main-view.fxml"));
+        this.stage = stage;
 
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        stage.setMinWidth(730);
         stage.setTitle("نهج الفصاحه");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
